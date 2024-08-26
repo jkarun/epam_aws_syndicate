a2e4fffae2cd30f1c9cf94c8c10e040b75b8d487
arun_prasath <jkarun63@gmail.com>
2024-08-26 18:51:18 +0530

    response change

--- task01/tests/test_hello_world/test_success.py
+++ task01/tests/test_hello_world/test_success.py
@@ -4,5 +4,8 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
-
+        response = {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        }
+        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), response)
