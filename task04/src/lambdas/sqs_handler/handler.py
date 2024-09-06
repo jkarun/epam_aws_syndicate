33631282240dab1d4ed7812960b50545fb3ee8d5
arun_prasath <jkarun63@gmail.com>
2024-09-06 20:10:51 +0530

    publish_version false

--- task04/src/lambdas/sqs_handler/handler.py
+++ task04/src/lambdas/sqs_handler/handler.py
@@ -13,7 +13,8 @@ class SqsHandler(AbstractLambda):
         """
         Explain incoming event here
         """
-        _LOG.info(event['Records'])
+        _LOG.info(event)
+        _LOG.info(event.get('Records', 'empty record received.'))
         _LOG.info(context)
         return 200
     
